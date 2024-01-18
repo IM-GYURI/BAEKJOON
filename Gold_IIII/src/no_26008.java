76194ac79f587f9b64dae9b9496599b07334a018
IM-GYURI <kgulr0517@gmail.com>
2024-01-18 22:00:42 +0900

    no.26008 clear

---
+++ Gold_IIII/src/no_26008.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class no_26008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long answer = 1L;
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int a = sc.nextInt();
+        int h = sc.nextInt();
+
+        for (int i = 0; i < n - 1; i++) {
+            answer = (answer * m) % 1000000007;
+        }
+        System.out.println(answer);
+    }
+}
