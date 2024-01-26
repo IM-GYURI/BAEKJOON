d0446e3902b647744bfa3ebe912ddda754064793
IM-GYURI <kgulr0517@gmail.com>
2024-01-27 01:43:08 +0900

    no.2830 clear

--- Gold_IIII/src/no_2830.java
+++ Gold_IIII/src/no_2830.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class no_2830 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
