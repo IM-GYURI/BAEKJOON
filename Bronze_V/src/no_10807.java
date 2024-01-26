2ba27bdf5566afb722b26aa0e487551a4ff84c91
IM-GYURI <kgulr0517@gmail.com>
2024-01-27 01:44:44 +0900

    no.10807 clear

---
+++ Bronze_V/src/no_10807.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Hashtable;
+import java.util.StringTokenizer;
+
+public class no_10807 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int count = 0;
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Hashtable<Integer, Integer> counts = new Hashtable<>();
+
+        for (int num : arr) {
+            if (counts.containsKey(num)) {
+                counts.put(num, counts.get(num) + 1);
+            } else {
+                counts.put(num, 1);
+            }
+        }
+
+        int target = Integer.parseInt(br.readLine());
+        if (counts.containsKey(target)) {
+            System.out.println(counts.get(target));
+        } else {
+            System.out.println("0");
+        }
+    }
+}
